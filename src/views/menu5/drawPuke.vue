27dd534aab92a8281bca384980170e29dffda2f9
tianlin <953108373@qq.com>
2018-11-11 10:52:07 +0800

    puke

--- src/views/menu5/drawPuke.vue
+++ src/views/menu5/drawPuke.vue
@@ -1,10 +1,8 @@
 <template>
   <div class="draw-puke">
-    <input type="range" name="pin" />
-    <div class="puke">
+    <div class="puke" v-for="(item, index) in pukes" :key="index" :style="{color: getColor(item.type)}">
       <div class="left-top">
-        <div class="value">
-        </div>
+        <div class="value">{{ item.value}}</div>
         <div class="type">
           <span v-if="item.type === '1'">♠</span>
           <span v-else-if="item.type === '2'">♥</span>
@@ -12,7 +10,15 @@
           <span v-else-if="item.type === '4'">♣</span>
         </div>
       </div>
-
+      <div class="right-bottom">
+        <div class="type">
+          <span v-if="item.type === '1'">♠</span>
+          <span v-else-if="item.type === '2'">♥</span>
+          <span v-else-if="item.type === '3'">♦</span>
+          <span v-else-if="item.type === '4'">♣</span>
+        </div>
+        <div class="value">{{ item.value}}</div>
+      </div>
     </div>
   </div>
 </template>
@@ -20,10 +26,30 @@
 <script>
   export default {
     data () {
+      let pukes = []
+      let types = ['1', '2', '3', '4']
+      let values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+      values.forEach(value => {
+        types.forEach(type => {
+          let puke = {}
+          puke.type = type
+          puke.value = value
+          pukes.push(puke)
+        })
+      })
       return {
-        item: {
-          type: '1',
-          value: 'A'
+        pukes: pukes
+      }
+    },
+    computed: {
+
+    },
+    methods: {
+      getColor (type) {
+        if (type === '1' || type === '4') {
+          return 'black'
+        } else {
+          return 'red'
         }
       }
     }
@@ -34,18 +60,41 @@
   .draw-puke {
     width: 100%;
     .puke {
-      border: 1px solid gainsboro;
-      width: 360px;
-      height: 540px;
-      .left-top {
+      margin: 8px;
+      border: 1px solid gray;
+      width: 270px;
+      height: 440px;
+      display: inline-block;
+      position: relative;
+
+      .right-bottom {
+        position: absolute;
+        bottom: 0;
+        width: 100%;
 
         .value {
-
+          font-size: 35px;
+          transform: rotate(180deg);
+          margin-right: 16px;
         }
 
         .type {
-          margin-left: 10px;
-          font-size: 20px;
+          transform: rotate(180deg);
+          margin-right: 16px;
+          text-align: left;
+          font-size: 35px;
+        }
+      }
+      .left-top {
+
+        .value {
+          font-size: 35px;
+          margin-left: 16px;
+        }
+
+        .type {
+          font-size: 35px;
+          margin-left: 16px;
         }
       }
     }
